e06e41b3702bd9afc409568d9abe1f11423f42b5
tktkaj <tktkaj@soongsil.ac.kr>
2024-10-13 15:52:40 +0900

    feat: styled-compontent 사용해 컴포넌트화

--- src/alarm/AlarmList.jsx
+++ src/alarm/AlarmList.jsx
@@ -1,47 +1,106 @@
 import React from 'react'
 import ProfileImg from '../images/test.png'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  width: 450px;
+  background-color: #F8F8FA;
+  padding-left: 30px;
+  height: 100vh;
+`;
+
+const Title = styled.div`
+  font-size: 1.8rem;
+  padding-top: 40px;
+  padding-bottom: 40px;
+`;
+
+const AlarmItem = styled.div`
+  display: flex;
+  padding-bottom: 25px;
+`;
+
+const ProfileImage = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+`;
+
+const AlarmTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+  width: 350px;
+  justify-content: center;
+  height: 60px;
+  gap: 5px;
+`;
+
+const AlarmText = styled.div`
+  width: 310px;
+  color: #333;
+  font-size: 1rem;
+`;
+
+const AlarmTime = styled.div`
+  color: #696969;
+  font-size: 0.8rem;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
+
+const AcceptButton = styled.button`
+  background-color: #706EF4;
+  padding: 5px 30px;
+  border-radius: 5px;
+  color: #ffffff;
+  margin-right: 15px;
+  font-size: 0.9rem;
+`;
+
+const DeclineButton = styled.button`
+  background-color: #D9D9D9;
+  padding: 5px 30px;
+  border-radius: 5px;
+  color: #333;
+  font-size: 0.9rem;
+`;
 
 export default function AlarmList() {
     return (
-        <div style={{ width: '450px', backgroundColor: '#F8F8FA', paddingLeft: '30px', height: '100vh'}}>
+        <Container>
 
-            <div style={{ fontSize: '1.8rem', paddingTop: '40px', paddingBottom: '40px' }}>알림</div>
+            <Title>알림</Title>
 
-            <div style={{ display: 'flex',paddingBottom: '20px'  }}>
+            <AlarmItem>
+                <ProfileImage src={ProfileImg} />
+                <AlarmTextContainer>
+                    <AlarmText>dduddo님이 회원님의 게시물을 좋아합니다.</AlarmText>
+                    <AlarmTime>방금 전</AlarmTime>
+                </AlarmTextContainer>
+            </AlarmItem>
 
-                <div>
-                    <img src={ProfileImg} style={{ width: '60px', height: '60px', borderRadius: '50%'}}></img>
-                </div>
+            <AlarmItem>
+                <ProfileImage src={ProfileImg} />
+                <AlarmTextContainer>
+                    <AlarmText>Yebook님이 회원님의 게시물에 댓글을 남겼습니다.</AlarmText>
+                    <AlarmTime>방금 전</AlarmTime>
+                </AlarmTextContainer>
+            </AlarmItem>
 
-                <div style={{display: 'flex', flexDirection: 'column', marginLeft: '20px', width:'350px', justifyContent:'center', height: '60px' }}>
+            <AlarmItem>
+                <ProfileImage src={ProfileImg} />
+                <AlarmTextContainer>
+                    <AlarmText>tktkaj님이 팔로우 요청하셨습니다.</AlarmText>
+                    <ButtonContainer>
+                        <AcceptButton>수락</AcceptButton>
+                        <DeclineButton>거절</DeclineButton>
+                    </ButtonContainer>
+                </AlarmTextContainer>
+            </AlarmItem>
 
-                    <div style={{ color: '#333', fontSize: '1rem'}}>dduddo님이 회원님의 게시물을 좋아합니다.</div>
-
-                    <div style={{ color: '#696969', fontSize: '0.8rem'}}>방금 전</div>
-                </div>
-
-            </div>
-
-            <div style={{ display: 'flex', paddingBottom: '40px' }}>
-
-                <div>
-                    <img src={ProfileImg} style={{ width: '60px', height: '60px', borderRadius: '50%' }}></img>
-                </div>
-
-                <div style={{display: 'flex', flexDirection: 'column', marginLeft: '20px', width:'350px', justifyContent:'center', height: '60px', gap:'5px'}}>
-
-                    <div style={{ color: '#333', fontSize: '1rem'}}>tktkaj님이 팔로우 요청하셨습니다.</div>
-
-                    <div style={{ display: 'flex'}}>
-
-                        <button style={{backgroundColor: '#706EF4', padding:'5px 30px', borderRadius:'5px', color: '#ffffff', marginRight:'15px', fontSize:'0.9rem'}}>수락</button>
-                        <button style={{backgroundColor: '#D9D9D9', padding:'5px 30px', borderRadius:'5px', color: '#333', fontSize:'0.9rem'}}>거절</button>
-
-                    </div>
-                </div>
-            </div>
-
-        </div>
+        </Container>
     )
 }
