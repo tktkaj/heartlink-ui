7cc02a580c5028c4b4dc48c186c7969c028dada9
tktkaj <tktkaj@soongsil.ac.kr>
2024-10-13 17:51:27 +0900

    feat: DM message CSS 적용

--- src/dm/MessageBubble.jsx
+++ src/dm/MessageBubble.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
-import TestImg from '../image/sns/google_logo_icon_147282.png';
+import TestImg from '../images/test.png';
 
 
 const Message = styled.div`
-  font-size: 1.2rem;
+  font-size: 1.1rem;
   display: flex;
   max-width: 50%;
-  padding: 5px 10px;
+  padding: 10px 10px;
   margin-bottom: 5px;
   background-color: ${(props) => (props.isMine ? '#706EF4' : '#DDDDDD')};
   color: ${(props) => (props.isMine ? '#FFFFFF' : '#333')};
@@ -52,8 +52,8 @@ export default function MessageBubble({ message }) {
         </Message>
       </div>
       <div style={{ display: 'flex', justifyContent: 'start' }}>
-        <Message isMine={true} style={{ borderRadius: '5px 10px 10px 10px' }}>
-          <img src={TestImg} style={{width: '250px', height: '250px'}}></img>
+        <Message isMine={true} style={{ borderRadius: '10px 10px 10px 10px', padding: '0', backgroundColor: '#FFFFFF' }}>
+          <img src={TestImg} style={{maxWidth:'90%', maxHeight:'90%', borderRadius: '10px'}}></img>
         </Message>
       </div>
     </>
