4f684a40c480f7de6da213f6b639455c7badb766
moong <cdn02147@naver.com>
2024-10-18 12:12:29 +0900

    Update MiniSide.jsx

--- src/sideMenu/MiniSide.jsx
+++ src/sideMenu/MiniSide.jsx
@@ -9,6 +9,8 @@ import { LuSearch } from "react-icons/lu";
 import { IoSettingsOutline } from "react-icons/io5";
 import Setting from './Setting';
 import { Link } from 'react-router-dom';
+import SearchMenu from './SearchMenu';
+import AlarmMenu from './AlarmMenu';
 
 
 const MiniContainer = styled.div`
@@ -84,8 +86,23 @@ export default function MiniSide() {
     const openSetting = () => setIsSettingOpen(true);
     const closeSetting = () => setIsSettingOpen(false);
 
+    const [isMiniSideVisible, setIsMiniSideVisible] = useState(true);
+    const [isSearchMenuVisible, setIsSearchMenuVisible] = useState(true);
+    const [isAlarmMenuVisible, setIsAlarmMenuVisible] = useState(true);
+
+    const toggleSideMenuSearch = () => {
+        setIsAlarmMenuVisible(false);
+        setIsSearchMenuVisible(true);
+    };
+    const toggleSideMenuAlarm = () => {
+        setIsAlarmMenuVisible(true);
+        setIsSearchMenuVisible(false);
+    };
+
     return (
         <>
+            {isMiniSideVisible && isSearchMenuVisible && <SearchMenu toggleSideMenuSearch={toggleSideMenuSearch} />}
+            {isMiniSideVisible && isAlarmMenuVisible && <AlarmMenu toggleSideMenuAlarm={toggleSideMenuAlarm} />}
             <MiniContainer>
                 <Logostyle>
                     <Link to="/home">
@@ -95,12 +112,12 @@ export default function MiniSide() {
                 <Ulstyle>
                     <div>
                         <Liststyle to="/home"><IoHomeOutline className='icon' /></Liststyle>
-                        <Liststyle><LuSearch className='icon' /></Liststyle>
+                        <Liststyle onClick={toggleSideMenuSearch}><LuSearch className='icon' /></Liststyle>
                         <Liststyle to="/couple"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-box2-heart" viewBox="0 0 16 16">
                             <path d="M8 7.982C9.664 6.309 13.825 9.236 8 13 2.175 9.236 6.336 6.31 8 7.982" />
                             <path d="M3.75 0a1 1 0 0 0-.8.4L.1 4.2a.5.5 0 0 0-.1.3V15a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4.5a.5.5 0 0 0-.1-.3L13.05.4a1 1 0 0 0-.8-.4zm0 1H7.5v3h-6zM8.5 4V1h3.75l2.25 3zM15 5v10H1V5z" />
                         </svg></Liststyle>
-                        <Liststyle><FaRegBell className='icon' /></Liststyle>
+                        <Liststyle onClick={toggleSideMenuAlarm}><FaRegBell className='icon' /></Liststyle>
                         <Liststyle><AiOutlineMessage className='icon' /></Liststyle>
                         <Liststyle to="/mypage">
                             <ProfileThum>
