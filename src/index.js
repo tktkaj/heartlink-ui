3049ae3c2cef0fb8f05a52d02b187b37a0e00052
moong <cdn02147@naver.com>
2024-10-20 19:57:57 +0900

    feat: 404페이지

--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'react-toastify/dist/ReactToastify.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import MainPage from './main/MainPage';
-import Search from './search/Search';
-import ChatRoom from './dm/ChatRoom';
-import MyPage from './mypage/MyPage';
-import Login from './login/Login';
-import DmListBox from './dm/DmListBox';
-import Couple from './Couple';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "react-toastify/dist/ReactToastify.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainPage from "./main/MainPage";
+import Search from "./search/Search";
+import ChatRoom from "./dm/ChatRoom";
+import MyPage from "./mypage/MyPage";
+import Login from "./login/Login";
+import DmListBox from "./dm/DmListBox";
+import Couple from "./Couple";
+import NotFound from "./main/NotFound";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       { index: true, element: <MainPage /> },
-      { path: '/home', element: <MainPage /> },
-      { path: '/search', element: <Search /> },
-      { path: '/couple', element: <Couple /> },
-      { path: '/dm', element: <ChatRoom /> },
-      { path: '/mypage', element: <MyPage /> },
-      { path: '/login', element: <Login /> },
-      { path: '/dmlistbox', element: <DmListBox /> },
-
+      { path: "/home", element: <MainPage /> },
+      { path: "/search", element: <Search /> },
+      { path: "/couple", element: <Couple /> },
+      { path: "/dm", element: <ChatRoom /> },
+      { path: "/mypage", element: <MyPage /> },
+      { path: "/login", element: <Login /> },
+      { path: "/dmlistbox", element: <DmListBox /> },
+      { path: "/*", element: <NotFound /> },
     ],
   },
 ]);
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
